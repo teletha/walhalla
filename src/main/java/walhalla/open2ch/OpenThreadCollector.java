61630d757f05aa624ed07060034d29f9c1fffc17
Teletha <Teletha.T@gmail.com>
2025-09-22 16:29:07 +0900

    wip

--- src/main/java/walhalla/open2ch/OpenThreadCollector.java
+++ src/main/java/walhalla/open2ch/OpenThreadCollector.java
@@ -55,7 +55,7 @@ public class OpenThreadCollector {
     }
 
     public static void main(String[] args) {
-        crawlByURL(15635, 1758414422);
+        crawlByURL(15637, 1758486650);
     }
 
     public static synchronized void crawlByURL(int num, int id) {
